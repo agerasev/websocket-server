06e36121cd943f28975ec5659888d8031267c8cc
Alex <alexey.gerasev@gmail.com>
2016-02-11 13:40:36 +0600

    use app default db

--- start-local.sh
+++ start-local.sh
@@ -1,1 +1,1 @@
-IP='localhost' PORT=8080 WSPORT=8000 MONGODB_URL='mongodb://localhost:27017/' npm start
+IP='localhost' PORT=8080 WSPORT=8000 MONGODB_URL='mongodb://localhost:27017/' MONGODB_DB='test' npm start
